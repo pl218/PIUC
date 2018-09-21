7fbdfb3135afb68ff774a18afb2d9bf753f4a0f8
inesamaro <ines.amaro.316@gmail.com>
2018-09-21 10:52:02 +0100

    Primeiro esboÃo do login

--- PIUC/urls.py
+++ PIUC/urls.py
@@ -17,8 +17,9 @@ ther URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
 ]
