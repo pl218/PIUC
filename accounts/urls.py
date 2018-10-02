f9da03e048c99b699c6d30990ace34af0d4730fc
jmartinhoj <ines.amaro.316@gmail.com>
2018-10-02 16:43:12 +0100

    changes to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name= "accounts/login.html"), name='login'),
     path('register/', views.register, name='register'),
     path('profile/',views.profile,name='profile'),
+    path('logout/',views.logout,name='profile'),
     path('feed/', include('feed.urls'))
 ]
