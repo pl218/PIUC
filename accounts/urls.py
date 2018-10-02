62eb2d84c7072c229d145fc758d0a2daad7eea45
jmartinhoj <ines.amaro.316@gmail.com>
2018-10-02 15:39:01 +0100

    navbar feed button working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LoginView
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name= "accounts/login.html"), name='login'),
     path('register/', views.register, name='register'),
     path('profile/',views.profile,name='profile'),
+    path('feed/', include('feed.urls'))
 ]
