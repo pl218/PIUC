bc2cd1968614939285d94d3c533eca362d286df7
José Luís Pereira <joseluispereira@din-wdei330.dei.uc.pt>
2018-10-30 18:16:46 +0000

    add models tests

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Post(models.Model):
-    user= models.ForeignKey(User,on_delete=models.CASCADE) #Many to one (User pode ter vários posts)
-    title=models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE) #Many to one (User pode ter vários posts)
+    title = models.CharField(max_length=100)
     post = models.CharField(max_length=500)
     date= models.DateTimeField(auto_now_add=True) #Creation date
     Edit_date= models.DateTimeField(auto_now=True) #last edit date
